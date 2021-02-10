444a1b85f42378e5dc4f2e9197a39840c5b69c77
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-02-10 18:26:11 +0100

    listen to the internet

--- backend/server.py
+++ backend/server.py
@@ -6,14 +6,14 @@ from backend.state import State
 
 state = State()
 
-@route('/<alias>/workon/<workingon>')
+
+@route("/<alias>/workon/<workingon>")
 def heartbeat(alias, workingon):
     """Receive heartbeat from alias, and return all available data"""
     state.add_client(alias, workingon, time.time())
     print(state)
     return json.dumps(state.get_working_clients())
 
-if __name__ == '__main__':
-    run(host='localhost', port=8333)
 
-
+if __name__ == "__main__":
+    run(host="0.0.0.0", port=8333)
