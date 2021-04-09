cd58d8d18304405629b21653ae2cc25d50621f07
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-04-09 18:00:34 +0200

    Introduce Config record. Partially implement green path test

--- client-purs/test/Main.purs
+++ client-purs/test/Main.purs
@@ -13,38 +13,58 @@ import Data.Traversable (for_)
 data WorkonEffect
   = Print String
   | CreateFile String (Array String)
+  | SetHeartbeatUrl String
+  | StartProcess (Array String)
 
 type Projectname
   = String
+
+type Config
+  = { cmdLine :: Array String, server :: String }
 
 instance showWorkonEffect :: Show WorkonEffect where
   show we = case we of
     Print s -> "Print " <> show s
     CreateFile s lines -> "CreateFile " <> show s <> " " <> show lines
+    SetHeartbeatUrl s -> "SetHeartbeatUrl " <> s
+    StartProcess parts -> "StartProcess" <> show parts
 
 derive instance eqWorkonEffect :: Eq WorkonEffect
 
-parse :: Array String -> String -> (Projectname -> Maybe {}) -> Array WorkonEffect
+exampleConfig :: Maybe Config
+exampleConfig =
+  Just
+    { cmdLine: [ "goland", "rescue" ]
+    , server: "http://212.47.253.51:8335"
+    }
+
+parse :: Array String -> String -> (Projectname -> Maybe Config) -> Array WorkonEffect
 parse [ "--create" ] _ _ = [ Print "Create what? --create by itself makes no sense..." ]
 
-parse [ "--create", projectName ] username readConfig = parse [projectName, "--create"] username readConfig
-parse [ projectName, "--create" ] username readConfig =
-  case readConfig projectName of
-    Nothing -> [ CreateFile
-          (projectName <> ".ini")
-          [ "[workon]"
-          , "cmdline=goland '/path/to the/project'"
-          , "server=http://212.47.253.51:8335"
-          , "user=" <> username
-          ]
-      , Print $ "'" <> projectName <> ".ini' created."
-      , Print "Open it with your favorite text editor then type"
-      , Print $ "   workon " <> projectName
-      , Print "again to begin samkoding!"
-      ]
-    Just _ -> [Print $ "'" <> projectName <> ".ini' already exists, cannot create!"]
+parse [ "--create", projectName ] username readConfig = parse [ projectName, "--create" ] username readConfig
 
-parse [ projectName ] _ _ = [ Print $ "Did not find '" <> projectName <> ".ini'. Re-run with flag --create to create a default!" ]
+parse [ projectName, "--create" ] username readConfig = case readConfig projectName of
+  Nothing ->
+    [ CreateFile
+        (projectName <> ".ini")
+        [ "[workon]"
+        , "cmdline=goland '/path/to the/project'"
+        , "server=http://212.47.253.51:8335"
+        ]
+    , Print $ "'" <> projectName <> ".ini' created."
+    , Print "Open it with your favorite text editor then type"
+    , Print $ "   workon " <> projectName
+    , Print "again to begin samkoding!"
+    ]
+  Just _ -> [ Print $ "'" <> projectName <> ".ini' already exists, cannot create!" ]
+
+parse [ projectName ] _ readConfig = case readConfig projectName of
+  Nothing -> [ Print $ "Did not find '" <> projectName <> ".ini'. Re-run with flag --create to create a default!" ]
+  Just s ->
+    [ Print $ "Working on " <> projectName <> ". Command line: goland rescue"
+    , SetHeartbeatUrl $ "http://212.47.253.51:8335/olof/workon/" <> projectName
+    , StartProcess [ "goland", "/home/olof/github/rescue" ]
+    ]
 
 parse _ _ _ = [ Print "Usage: workon <projname>" ]
 
@@ -63,17 +83,17 @@ main =
 
               readConfig _ = Nothing
             parse cmdLine user readConfig `shouldEqual` expected
-          for_ ["rescue", "polarbear"] \projectName -> do
-              it ("prints error message when config cannot be found for " <> projectName) do
-                let
-                  expected = [ Print $ "Did not find '" <> projectName <> ".ini'. Re-run with flag --create to create a default!" ]
+          for_ [ "rescue", "polarbear" ] \projectName -> do
+            it ("prints error message when config cannot be found for " <> projectName) do
+              let
+                expected = [ Print $ "Did not find '" <> projectName <> ".ini'. Re-run with flag --create to create a default!" ]
 
-                  cmdLine = [ projectName ]
+                cmdLine = [ projectName ]
 
-                  user = "olof"
+                user = "olof"
 
-                  readConfig _ = Nothing
-                parse cmdLine user readConfig `shouldEqual` expected
+                readConfig _ = Nothing
+              parse cmdLine user readConfig `shouldEqual` expected
           it "prints error message when user writes create without project name" do
             let
               expected = [ Print "Create what? --create by itself makes no sense..." ]
@@ -84,41 +104,56 @@ main =
 
               readConfig _ = Nothing
             parse cmdLine user readConfig `shouldEqual` expected
-          for_ ["rescue", "polarbear"] \projectName -> it ("prints error message when user tries to create already existing project " <> projectName) do
-            let
-              expected = [ Print $ "'" <> projectName <> ".ini' already exists, cannot create!" ]
+          for_ [ "rescue", "polarbear" ] \projectName ->
+            it ("prints error message when user tries to create already existing project " <> projectName) do
+              let
+                expected = [ Print $ "'" <> projectName <> ".ini' already exists, cannot create!" ]
 
-              cmdLine = [ "--create", projectName ]
+                cmdLine = [ "--create", projectName ]
 
-              user = "samuel"
+                user = "samuel"
 
-              readConfig _ = Just {}
-            parse cmdLine user readConfig `shouldEqual` expected
-          for_ ["samuel", "tor"] \username -> do
-            for_ ["ijpurs", "polarbear"] \projectName -> do
-              for_ [true, false] \reverseCmdLine -> do
-                  it ("writes correct config file for user " <> username <> " project " <> projectName <> " when create flag is supplied") do
-                    let
-                      expected =
-                        [ CreateFile
-                            (projectName <> ".ini")
-                            [ "[workon]"
-                            , "cmdline=goland '/path/to the/project'"
-                            , "server=http://212.47.253.51:8335"
-                            , "user=" <> username
-                            ]
-                        , Print $ "'" <> projectName <> ".ini' created."
-                        , Print "Open it with your favorite text editor then type"
-                        , Print $ "   workon " <> projectName
-                        , Print "again to begin samkoding!"
-                        ]
+                readConfig _ = exampleConfig
+              parse cmdLine user readConfig `shouldEqual` expected
+          for_ [ "samuel", "tor" ] \username -> do
+            for_ [ "ijpurs", "polarbear" ] \projectName -> do
+              for_ [ true, false ] \reverseCmdLine -> do
+                it ("writes correct config file for user " <> username <> " project " <> projectName <> " when create flag is supplied") do
+                  let
+                    expected =
+                      [ CreateFile
+                          (projectName <> ".ini")
+                          [ "[workon]"
+                          , "cmdline=goland '/path/to the/project'"
+                          , "server=http://212.47.253.51:8335"
+                          ]
+                      , Print $ "'" <> projectName <> ".ini' created."
+                      , Print "Open it with your favorite text editor then type"
+                      , Print $ "   workon " <> projectName
+                      , Print "again to begin samkoding!"
+                      ]
 
-                      cmdLine = if reverseCmdLine then [ projectName, "--create" ] else ["--create", projectName]
+                    cmdLine = if reverseCmdLine then [ projectName, "--create" ] else [ "--create", projectName ]
 
-                      user = username
+                    user = username
 
-                      readConfig _ = Nothing
-                    parse cmdLine user readConfig `shouldEqual` expected
+                    readConfig _ = Nothing
+                  parse cmdLine user readConfig `shouldEqual` expected
+          for_ [ "rescue", "polarbear" ] \projectName -> do
+            it ("running script when config exists for project " <> projectName <> ", user olof") do
+              let
+                expected =
+                  [ Print $ "Working on " <> projectName <> ". Command line: goland rescue"
+                  , SetHeartbeatUrl $ "http://212.47.253.51:8335/olof/workon/" <> projectName
+                  , StartProcess [ "goland", "/home/olof/github/rescue" ]
+                  ]
+
+                cmdLine = [ projectName ]
+
+                user = "olof"
+
+                readConfig _ = exampleConfig
+              parse cmdLine user readConfig `shouldEqual` expected
 
 pythonUnitTest :: String
 pythonUnitTest =
