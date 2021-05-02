6c461367b03897d5efe5b85bf7dff2f434e1fcac
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-05-02 14:24:19 +0200

    Extract eval of command to toplevel function

--- client-purs/src/Main.purs
+++ client-purs/src/Main.purs
@@ -1,7 +1,6 @@
 module Main where
 
 import Prelude
-
 import Effect (Effect)
 import Effect.Console (log)
 import Node.Process
@@ -13,10 +12,12 @@ import Data.Foldable
 configReader :: Projectname -> Maybe Config
 configReader _ = Nothing
 
+eval (Print s) = log s
+eval command = log $ show command
+
 main :: Effect Unit
 main = do
   toParse <- drop 2 <$> argv
-  let parsed = parse toParse "samuel" configReader
-  for_ parsed \command -> case command of
-    Print s -> log s
-    _ -> log $ show command
+  let
+    parsed = parse toParse "samuel" configReader
+  for_ parsed eval
