8c7bef46479dd03231f5ebda92d8874bb882c066
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-05-02 14:14:38 +0200

    Remove first 2 unuseful arguments before parsing

--- client-purs/src/Main.purs
+++ client-purs/src/Main.purs
@@ -7,13 +7,14 @@ import Effect.Console (log)
 import Node.Process
 import Lib
 import Data.Maybe
+import Data.Array
 
 configReader :: Projectname -> Maybe Config
 configReader _ = Nothing
 
 main :: Effect Unit
 main = do
-  toParse <- argv
+  toParse <- drop 2 <$> argv
   let parsed = parse toParse "samuel" configReader
   case parsed of
     [Print s] -> log s
